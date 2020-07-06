ff7167c81c5faa4d6d4174edd107c6fd20759514
Mantas Misiunas <mantaxxxas@gmail.com>
2020-07-06 11:39:41 +0300

    Added path to every blog

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,11 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.body[:100]
+    def pub_date_short(self):
+        return self.pub_date.strftime('%b %e %Y')

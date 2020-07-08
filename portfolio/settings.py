fb492cda600923b9d06950925b1ee72aaaae399e
Mantas Misiunas <mantaxxxas@gmail.com>
2020-07-08 18:48:39 +0300

    final

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dch7ngk#0_@pex9o5_l4#3*v8&kok-ydjb=is1-871%8&p#i9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
